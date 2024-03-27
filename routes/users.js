86d08230e43462029daa8a3bfe4043cea08d00da
xieshuqiang <shuqiangxie@outlook.com>
2024-03-28 00:22:04 +0800

    database part 1

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/**添加/users/cool路由 */
+router.get('/cool',function(req,res,next) {
+  res.send("你好cool");
+})
+
 module.exports = router;
